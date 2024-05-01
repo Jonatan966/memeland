0432cfb1db0700ff9fa4c0e7feef057c1ea02dd2
Jonatan Frederico <jonatanfrederico@gmail.com>
2024-05-01 17:02:35 -0300

    feat: set able to copy meme in home page

--- web/src/components/domain/meme-card.tsx
+++ web/src/components/domain/meme-card.tsx
@@ -1,28 +1,52 @@
 import { Meme } from "@/services/supabase";
 import { Badge } from "../ui/badge";
+import { Button } from "../ui/button";
+import { FileIcon } from "@radix-ui/react-icons";
+import { useMeme } from "@/hooks/use-meme";
 
 interface MemeCardProps {
   meme: Meme;
+  onSelect(): void;
 }
 
-export function MemeCard({ meme }: MemeCardProps) {
+export function MemeCard({ meme, onSelect }: MemeCardProps) {
   const fileUrl = `${
     import.meta.env.VITE_SUPABASE_URL
   }/storage/v1/object/public/memes/${meme.file}`;
 
+  const {
+    memeHasCopySupport,
+    memeTypeLabel,
+    onCopyMemeContent,
+    onCopyMemeLink,
+  } = useMeme(fileUrl);
+
   return (
     <div
-      className="relative cursor-pointer border hover:border-green-500 mb-4"
-      onClick={() => alert("card")}
+      className="relative cursor-pointer border hover:border-green-500 mb-4 group"
+      onClick={onSelect}
     >
       <img src={fileUrl} alt="Photo" className="w-full aspect-square" />
 
       <div className="absolute inset-0 flex items-start">
-        <Badge>Imagem</Badge>
+        <Badge>{memeTypeLabel}</Badge>
       </div>
-      {/* <div className="absolute inset-0 opacity-0 hover:opacity-100">
-        <button>Botão</button>
-      </div> */}
+      <div className="absolute bottom-1 left-1 right-1 opacity-0 group-hover:opacity-100 flex gap-1">
+        {memeHasCopySupport && (
+          <Button
+            size="icon"
+            variant="secondary"
+            className="h-8"
+            onClick={onCopyMemeContent}
+          >
+            <FileIcon />
+          </Button>
+        )}
+
+        <Button size="sm" className="w-full h-8" onClick={onCopyMemeLink}>
+          Copiar link do meme
+        </Button>
+      </div>
     </div>
   );
 }
