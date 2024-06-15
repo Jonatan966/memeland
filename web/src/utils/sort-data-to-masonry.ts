3ab38104819f44eb4ba1450e39cf52a312262ba2
Jonatan Frederico <jonatanfrederico@gmail.com>
2024-06-14 21:28:05 -0300

    feat(web): add util to sort data to masonry

---
+++ web/src/utils/sort-data-to-masonry.ts
@@ -0,0 +1,19 @@
+export function sortDataToMasonry<T>(
+  data: T[],
+  columns: number,
+  getDummy: () => T,
+) {
+  const newList: T[] = [];
+
+  const rows = Math.floor(data.length / columns);
+
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      const targetMeme = data[j * columns + i];
+
+      newList.push(targetMeme || getDummy());
+    }
+  }
+
+  return newList;
+}
