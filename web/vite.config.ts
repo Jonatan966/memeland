92935b924584b219ded17fe0fba32f3b76b055d5
Jonatan Frederico <jonatanfrederico@gmail.com>
2024-08-23 22:49:05 -0300

    fix(web): read env vars in vite config correctly

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,22 +1,27 @@
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const STORAGE_BASE = process.env.STORAGE_BASE || env.STORAGE_BASE;
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  server: {
-    proxy: {
-      '/media': {
-        target: process.env.STORAGE_BASE,
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace('/media', '')
+    server: {
+      proxy: {
+        '/media': {
+          target: STORAGE_BASE,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace('/media', '')
+        }
       }
     }
   }
