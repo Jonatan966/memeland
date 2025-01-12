4f3351a5ca4e2528d2b0cc622131d50f12801f86
Jonatan Frederico <jonatanfrederico@gmail.com>
2025-01-12 03:20:46 -0300

    feat: create github service

--- worker/worker-configuration.d.ts
+++ worker/worker-configuration.d.ts
@@ -17,4 +17,7 @@ type Env = {
   OPENAI_API_KEY: string;
   CLOUDFLARE_ACCOUNT_ID: string;
   CLOUDFLARE_API_KEY: string;
+  GITHUB_CLIENT_ID: string;
+  GITHUB_CLIENT_SECRET: string;
+  GITHUB_REDIRECT_URI: string;
 };
