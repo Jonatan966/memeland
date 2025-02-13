d6b6669bca6f177b1b726855a9ed4812330691c3
Jonatan Frederico <jonatanfrederico@gmail.com>
2025-02-12 22:37:15 -0300

    feat(web): implement auth process

--- web/src/contexts/AuthContext.tsx
+++ web/src/contexts/AuthContext.tsx
@@ -1,8 +1,10 @@
 import { User, workerService } from "@/services/worker";
 import { createContext, ReactNode, useEffect, useState } from "react";
 import { toast } from "sonner";
-import { SunIcon } from "@radix-ui/react-icons";
+import { SunIcon, GitHubLogoIcon } from "@radix-ui/react-icons";
 import { Button } from "@/components/ui/button";
+import { mountGithubAuthUrl } from "@/utils/mount-github-auth-url";
+import { setUrlSearchParams } from "@/utils/set-url-search-params";
 
 interface AuthContextProps {
   user?: User;
@@ -10,19 +12,36 @@ interface AuthContextProps {
 
 const AuthContext = createContext({} as AuthContextProps);
 
+const searchParams = new URLSearchParams(window.location.search);
+
 export function AuthProvider(props: { children: ReactNode }) {
   const [isLoadingSession, setIsLoadingSession] = useState(true);
   const [user, setUser] = useState<User>();
 
   useEffect(() => {
-    workerService
-      .getUserInfo()
-      .then((userInfo) => setUser(userInfo))
-      .catch((error) => {
+    async function processAuth() {
+      try {
+        const code = searchParams.get("code");
+
+        if (code) {
+          await workerService.authenticate(code);
+        }
+
+        const userInfo = await workerService.getUserInfo();
+
+        setUser(userInfo);
+      } catch (error) {
         console.log(error);
         toast.error("Não foi possível fazer login no momento :(");
-      })
-      .finally(() => setIsLoadingSession(false));
+
+        const newUrl = setUrlSearchParams(window.location.href, {});
+        window.history.pushState({ path: newUrl }, "", newUrl);
+      }
+
+      setIsLoadingSession(false);
+    }
+
+    processAuth();
   }, []);
 
   if (isLoadingSession) {
@@ -37,7 +56,8 @@ export function AuthProvider(props: { children: ReactNode }) {
   if (!user) {
     return (
       <div className="absolute inset-0 flex flex-col items-center justify-center">
-        <Button onClick={() => (window.location.href = "https://ojonatan.dev")}>
+        <Button onClick={() => (window.location.href = mountGithubAuthUrl())}>
+          <GitHubLogoIcon className="mr-1" />
           Login com o Github
         </Button>
       </div>
