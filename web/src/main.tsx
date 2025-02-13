e40a32b1e994ea79c8da22ec2b5cd2421d5b348e
Jonatan Frederico <jonatanfrederico@gmail.com>
2025-02-12 22:36:40 -0300

    fix(web): remove react strict mode

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
 import { Toaster } from "./components/ui/sonner.tsx";
@@ -7,10 +6,10 @@ import { AuthProvider } from "./contexts/AuthContext.tsx";
 import "./globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <>
     <AuthProvider>
       <App />
     </AuthProvider>
     <Toaster />
-  </React.StrictMode>
+  </>
 );
